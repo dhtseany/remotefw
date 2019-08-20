e450cadf3c322298e932b39b54d6767e64dc9237
Sean Snell <snellsg@gmail.com>
2019-08-20 18:28:19 -0400

    20AUG2019 Commit

--- client/services_remotefw.php
+++ client/services_remotefw.php
@@ -1,31 +1,41 @@
 <?php
-
+/*
 # This page will serve as the dev platform for pfSense and OPNSense.
 
 ## Based on info from:
 ## https://github.com/pfsense/pfsense-packages/blob/master/config/siproxd/siproxd_registered_phones.php
+*/
 
+/*
 pfSense_MODULE:	shell
 */
+
 ##|+PRIV
-##|*IDENT=page-status-remotefw
+##|*IDENT=page-services-remotefw
 ##|*NAME=Status: remotefw central management agent
 ##|*DESCR=Allow access to the 'Status: remotefw management page' page.
 ##|*MATCH=remotefw.php*
 ##|-PRIV
+
 require_once("guiconfig.inc");
 
-if (file_exists("/var/remotefw/main.cfg")) {
-	$phonetext = file_get_contents("/var/siproxd/siproxd_registrations");
-	$phonedata = explode("\n", $phonetext);
+if (file_exists("/etc/remotefw/main.cfg")) {
+	$configRec = file_get_contents("/etc/remotefw/main.cfg");
+	$config = explode("\n", $configRec);
 }
 
+$pgtitle = array(gettext("Services"), gettext("remoteFw"));
+
 require("head.inc");
+
 ?>
 
 <body link="#0000CC" vlink="#0000CC" alink="#0000CC">
+
 <?php include("fbegin.inc"); ?>
+
 Stuff goes here.
+
 <?php include("fend.inc"); ?>
 </body>
 </html>
