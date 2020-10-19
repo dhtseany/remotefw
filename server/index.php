6dfc2c43ac02c8a7fa0a24cfbd2640a50906105e
Sean Snell <ssnell@cmhsol.com>
2020-10-19 15:51:18 -0400

    Server index update

--- server/index.php
+++ server/index.php
@@ -1,9 +1,15 @@
+<?php
+testVar = $_GET['test'];
+?>
+
 <html>
 <head>
 <title>RemoteFW :: Server Console</title>
 </head>
 <body>
 <h1>RemoteFW :: Server console.</h1>
-
+<?php
+echo "[Test]" $testVar; 
+?>
 </body>
 </html>
