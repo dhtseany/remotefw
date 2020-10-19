873b7b1e0a9a17366143a07fb7bbb5b5c1d166c7
Sean Snell <ssnell@cmhsol.com>
2020-10-19 16:30:17 -0400

    Server index SQL testing

--- server/index.php
+++ server/index.php
@@ -1,6 +1,13 @@
 <?php
 require('config/database.php');
 $testVar = $_GET['test'];
+$testVar2 = "static_garbage"
+$insert_query = "INSERT INTO testTable(`testVar`,`testVar2`) VALUES 
+(
+'". mysqli_real_escape_string($mysqli_link, 'testVar') ."',
+'". mysqli_real_escape_string($mysqli_link, 'testVar2') ."'
+)";
+
 ?>
 
 <html>
@@ -9,8 +16,20 @@ $testVar = $_GET['test'];
 </head>
 <body>
 <h1>RemoteFW :: Server console.</h1>
+
 <?php
+
+// run the insert query
+If (mysqli_query($mysqli_link, $insert_query)) {
+    echo 'Record inserted successfully.';
+}
+
 echo "[Test] $testVar"; 
 ?>
+
 </body>
 </html>
+<?php
+// close the db connection
+mysqli_close($mysqli_link);
+?>
