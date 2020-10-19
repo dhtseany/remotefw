fbb4e3c1d8b5c6e8708d84acc34e3116f3e01ee4
Sean Snell <ssnell@cmhsol.com>
2020-10-19 15:44:00 -0400

    Server console work

--- server/index.php
+++ server/index.php
@@ -1,5 +1,11 @@
+
+<html>
+<head>
+<title>RemoteFW :: Server Console</title>
+</head>
+<body>
 <?php
-
 echo "[SERVER] This file is currently empty and is simply serving as a placeholder for future intentions.";
-
 ?>
+</body>
+</html>
