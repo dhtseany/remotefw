b5cd7cd2ea7514653e1475c92910a3b694c08225
Sean Snell <ssnell@cmhsol.com>
2020-10-20 15:58:23 -0400

    rfscraper dev

--- client/wip/rfscraper.php
+++ client/wip/rfscraper.php
@@ -1,5 +1,6 @@
 <?php
+$output = shell_exec('/usr/local/sbin/opnsense-version'); 
 
-echo "This file is currently empty and is simply serving as a placeholder for future intentions.";
+echo "[OUTPUT] $output";
 
 ?>
