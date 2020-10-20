25351846916296f4b7ed7aef05f68e8273cf2f34
Sean Snell <ssnell@cmhsol.com>
2020-10-20 18:23:29 -0400

    rfscraper added osVersion

--- client/wip/rfscraper.php
+++ client/wip/rfscraper.php
@@ -2,36 +2,40 @@
 $scriptAudit = "yes";
 
 $cpuArch = shell_exec("sysctl -a | egrep -i 'hw.machine_arch' | sed 's/.* //'");
+$wanIp = shell_exec("curl -s http://whatismyip.akamai.com/");
 
-echo "[ARCH] $cpuArch";
+echo "[Audit][ARCH] $cpuArch";
 
 if (file_exists("/etc/platform"))
     {
         if ($scriptAudit = "yes");
-            {echo "[Y] /etc/platform \n";}
+            {echo "[Audit][Y] /etc/platform \n";}
         $osType = shell_exec('cat /etc/platform');
     } 
     else
         {    
             if ($scriptAudit = "yes");
-            {echo "[N] /etc/platform \n";}
+            {echo "[Audit][N] /etc/platform \n";}
         }
 
 if (file_exists("/usr/local/sbin/opnsense-version"))
     {
         if ($scriptAudit = "yes");
-            {echo "[Y] /usr/local/sbin/opnsense-version \n";}
+            {echo "[Audit][Y] /usr/local/sbin/opnsense-version \n";}
         $osTypeOPNFull = shell_exec('/usr/local/sbin/opnsense-version');
         $osTypeOPNFullArray = explode(' ',trim($osTypeOPNFull));
         $osType = $osTypeOPNFullArray[0];
+        $osVersion = $osTypeOPNFullArray[1];
     }
     else
         {    
             if ($scriptAudit = "yes");
-                {echo "[N] /usr/local/sbin/opnsense-version \n";}
+                {echo "[Audit][N] /usr/local/sbin/opnsense-version \n";}
         }
+
 // Audit Checks
 if ($scriptAudit = "yes");
-{echo "[osType] $osType \n";}
+{echo "[Audit][osType] $osType \n";
+echo "[Audit][osVersion] " . $osVersion . "\n";}
 
 ?>
