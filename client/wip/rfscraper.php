822cb9eb5bc4bb3c7d0e6d5425c5a87ba43ea71b
Sean Snell <ssnell@cmhsol.com>
2020-10-20 18:02:00 -0400

    rfscraper code cleanup

--- client/wip/rfscraper.php
+++ client/wip/rfscraper.php
@@ -1,10 +1,8 @@
 <?php
 $scriptAudit = "yes";
 
-$opnsense = shell_exec('/usr/local/sbin/opnsense-version'); 
 $cpuArch = shell_exec("sysctl -a | egrep -i 'hw.machine_arch' | sed 's/.* //'");
 
-echo "[OPNSENSE] $opnsense";
 echo "[ARCH] $cpuArch";
 
 if (file_exists("/etc/platform"))
