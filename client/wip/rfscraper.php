fcbd7966cea99322d4a50dbd3e6fa7fcf774ace7
Sean Snell <ssnell@cmhsol.com>
2020-10-20 16:41:16 -0400

    rfscraper syntax corrections

--- client/wip/rfscraper.php
+++ client/wip/rfscraper.php
@@ -5,7 +5,7 @@ $cpuArch = shell_exec("sysctl -a | egrep -i 'hw.machine_arch' | sed 's/.* //'");
 echo "[OPNSENSE] $opnsense";
 echo "[ARCH] $cpuArch";
 
-if (file_exists(/etc/platform))
+if (file_exists("/etc/platform"))
     {echo "The file exists";} 
     else
     {echo "The file does not exist";}
