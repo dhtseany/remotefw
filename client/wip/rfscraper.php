ac197748a504cf41c6cd144fb3586b50e76986a2
Sean Snell <ssnell@cmhsol.com>
2020-10-20 17:59:22 -0400

    rfscraper improved opn detection

--- client/wip/rfscraper.php
+++ client/wip/rfscraper.php
@@ -23,15 +23,17 @@ if (file_exists("/usr/local/sbin/opnsense-version"))
     {
         if ($scriptAudit = "yes");
             {echo "[Y] /usr/local/sbin/opnsense-version";}
-        $osType = shell_exec('/usr/local/sbin/opnsense-version');
+        $osTypeOPNFull = shell_exec('/usr/local/sbin/opnsense-version');
+        $osTypeOPNFullArray = explode(' ',trim($osTypeOPNFull));
+        $osType = $osTypeOPNFullArray[0];
     }
     else
         {    
             if ($scriptAudit = "yes");
                 {echo "[N] /usr/local/sbin/opnsense-version";}
         }
-
+// Audit Checks
 if ($scriptAudit = "yes");
-{echo "[osType] $osType";}
+{echo "<pre>[osType] $osType</pre>";}
 
 ?>
