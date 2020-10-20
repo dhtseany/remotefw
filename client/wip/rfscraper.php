f0c059fbc19445c8e096b42a0dbd55ed29a14717
Sean Snell <ssnell@cmhsol.com>
2020-10-20 16:40:42 -0400

    rfscraper testing

--- client/wip/rfscraper.php
+++ client/wip/rfscraper.php
@@ -1,6 +1,13 @@
 <?php
-$output = shell_exec('/usr/local/sbin/opnsense-version'); 
+$opnsense = shell_exec('/usr/local/sbin/opnsense-version'); 
+$cpuArch = shell_exec("sysctl -a | egrep -i 'hw.machine_arch' | sed 's/.* //'");
 
-echo "[OUTPUT] $output";
+echo "[OPNSENSE] $opnsense";
+echo "[ARCH] $cpuArch";
+
+if (file_exists(/etc/platform))
+    {echo "The file exists";} 
+    else
+    {echo "The file does not exist";}
 
 ?>
