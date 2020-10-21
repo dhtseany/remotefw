f2d85d60786e6066fc1e0980e63a1ed1ad11dc21
Sean Snell <ssnell@cmhsol.com>
2020-10-21 14:35:16 -0400

    Server index removed nav name section

--- server/index.php
+++ server/index.php
@@ -166,12 +166,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="resources/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">RemoteFW</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
+      <!-- Sidebar user panel (optional)
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
           <img src="resources/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
@@ -179,7 +179,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
         </div>
-      </div>
+      </div> -->
 
       <!-- SidebarSearch Form
       <div class="form-inline">
