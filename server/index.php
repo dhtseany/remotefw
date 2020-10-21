09a657f7b9d644ceb059e215b8d00081bbe79a53
Sean Snell <ssnell@cmhsol.com>
2020-10-21 13:44:10 -0400

    Sever index page updates

--- server/index.php
+++ server/index.php
@@ -206,13 +206,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
-            <!-- <!-- <ul class="nav nav-treeview"> -->
-              <!-- <li class="nav-item">
+            <ul class="nav nav-treeview"> -->
+            <li class="nav-item">
                 <a href="#" class="nav-link active">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Active Page</p>
                 </a>
-              </li> -->
+              </li>
               <li class="nav-item">
                 <a href="clients.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
