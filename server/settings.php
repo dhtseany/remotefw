43b53d41cf02839a0dcf8bcbb137645cdf5cc132
Sean Snell <ssnell@cmhsol.com>
2020-10-22 15:18:57 -0400

    Added settings.php

---
+++ server/settings.php
@@ -0,0 +1,36 @@
+<?php
+$pageName="settings";
+$pageTitle="Settings";
+require('config/database.php');
+include('resources/structure/head.php');
+include('resources/structure/navbar.php');
+include('resources/structure/contenthead.php');
+navBarDisplay($pageName);
+displayContentHead($pageName, $pageTitle);
+?>
+
+<!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                <h1>Coming soon!</h1>
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+      </div>
+      <!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php
+include('resources/structure/sidebar.php');
+include('resources/structure/footer.php');
+include('resources/structure/scripts.php');
+
+// close the db connection
+mysqli_close($mysqli_link);
+?>
