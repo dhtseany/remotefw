918b99009c71121ff634ef39543951ed691d2bf0
Sean Snell <ssnell@cmhsol.com>
2020-10-22 15:53:00 -0400

    Server clients trimmed table code

--- server/clients.php
+++ server/clients.php
@@ -10,109 +10,66 @@ displayContentHead($pageName, $pageTitle);
 ?>
 
 <!-- Main content -->
-    <div class="content">
-      <div class="container-fluid">
+  <div class="content">
+    <div class="container-fluid">
       <div class="row">
-          <div class="col-md-12">
-            <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">Active Client Devices</h3>
-                <div class="card-tools">
-                  <ul class="pagination pagination-sm float-right">
-                    <li class="page-item"><a class="page-link" href="#">«</a></li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">»</a></li>
-                  </ul>
-                </div>
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Active Client Devices</h3>
+              <div class="card-tools">
+                <ul class="pagination pagination-sm float-right">
+                  <li class="page-item"><a class="page-link" href="#">«</a></li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">»</a></li>
+                </ul>
               </div>
-              <!-- /.card-header -->
-              <div class="card-body p-0">
-                <table class="table">
-                  <thead>
-                    <tr>
-                      <th style="width: 5%">cid</th>
-                      <th style="width: 20%">customer</th>
-                      <th style="width: 12.5%">location</th>
-                      <th style="width: 12.5%">osType</th>
-                      <th style="width: 12.5%">osArch</th>
-                      <th style="width: 12.5%">osVer</th>
-                      <th style="width: 12.5%">cpuCores</th>
-                      <th style="width: 12.5%">lastCheckInTime</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1.</td>
-                      <td>Update software</td>
-                      <td>
-                        <div class="progress progress-xs">
-                          <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-danger">55%</span></td>
-                      <td>#</td>
-                      <td>#</td>
-                      <td>#</td>
-                      <td>#</td>
-                    </tr>
-                    <tr>
-                      <td>2.</td>
-                      <td>Clean database</td>
-                      <td>
-                        <div class="progress progress-xs">
-                          <div class="progress-bar bg-warning" style="width: 70%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-warning">70%</span></td>
-                      <td>#</td>
-                      <td>#</td>
-                      <td>#</td>
-                      <td>#</td>
-                    </tr>
-                    <tr>
-                      <td>3.</td>
-                      <td>Cron job running</td>
-                      <td>
-                        <div class="progress progress-xs progress-striped active">
-                          <div class="progress-bar bg-primary" style="width: 30%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-primary">30%</span></td>
-                      <td>#</td>
-                      <td>#</td>
-                      <td>#</td>
-                      <td>#</td>
-                    </tr>
-                    <tr>
-                      <td>4.</td>
-                      <td>Fix and squish bugs</td>
-                      <td>
-                        <div class="progress progress-xs progress-striped active">
-                          <div class="progress-bar bg-success" style="width: 90%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-success">90%</span></td>
-                      <td>#</td>
-                      <td>#</td>
-                      <td>#</td>
-                      <td>#</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <!-- /.card-body -->
             </div>
-            <!-- /.card -->
+            <!-- /.card-header -->
+            <div class="card-body p-0">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th style="width: 5%">cid</th>
+                    <th style="width: 20%">customer</th>
+                    <th style="width: 12.5%">location</th>
+                    <th style="width: 12.5%">osType</th>
+                    <th style="width: 12.5%">osArch</th>
+                    <th style="width: 12.5%">osVer</th>
+                    <th style="width: 12.5%">cpuCores</th>
+                    <th style="width: 12.5%">lastCheckInTime</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1.</td>
+                    <td>Update software</td>
+                    <td>
+                      <div class="progress progress-xs">
+                        <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                      </div>
+                    </td>
+                    <td><span class="badge bg-danger">55%</span></td>
+                    <td>#</td>
+                    <td>#</td>
+                    <td>#</td>
+                    <td>#</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.card-body -->
           </div>
-          <!-- /.col -->
+          <!-- /.card -->
         </div>
-
-      
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
     </div>
-    <!-- /.content -->
+    <!-- /.container-fluid -->
   </div>
+  <!-- /.content -->
+</div>
 <!-- /.content-wrapper -->
 
 <?php
