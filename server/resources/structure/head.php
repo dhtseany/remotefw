206de1d260be932bedc6a42444e04ab08110ca78
Sean Snell <ssnell@cmhsol.com>
2020-10-24 17:46:36 -0400

    Added missing CSS from demo site

--- server/resources/structure/head.php
+++ server/resources/structure/head.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>RemoteFW :: Dashboard</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  
+
   <!-- Missing Fonts here, fix later -->
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="resources/plugins/fontawesome-free/css/all.min.css">
   <!-- Missing Fonts here, fix later -->
-  
+
   <!-- Theme style -->
   <link rel="stylesheet" href="resources/css/adminlte.min.css">
+  <link rel="stylesheet" href="resources/css/additional.css">
+
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
