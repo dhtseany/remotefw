db727f397de9631e5e025d336e0cd751ca6af685
Sean Snell <ssnell@cmhsol.com>
2020-10-24 14:21:55 -0400

    Server sf echo fix

--- server/resources/functions/shared_functions.php
+++ server/resources/functions/shared_functions.php
@@ -4,7 +4,7 @@ function displayCustomerName($custID, $mysqli_link) {
     $selectCustomer = "SELECT bizName FROM customers WHERE custID='$custID'";
     $resultCustomer = mysqli_query($mysqli_link, $selectCustomer);
     while ($customerRow = mysqli_fetch_array($resultCustomer, MYSQLI_ASSOC)) {
-        echo $resultCustomer["bizName"];
+        echo $customerRow["bizName"];
     }
 }
 
