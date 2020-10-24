a333a1babfcbf838b5e381675cf85517056f9536
Sean Snell <ssnell@cmhsol.com>
2020-10-24 14:15:36 -0400

    Server misc updates

--- server/resources/functions/shared_functions.php
+++ server/resources/functions/shared_functions.php
@@ -1,11 +1,13 @@
 <?php
 
 function displayCustomerName($custID) {
+    require('config/database.php');
     $selectCustomer = "SELECT bizName FROM customers WHERE custID='$custID'";
     $resultCustomer = mysqli_query($mysqli_link, $selectCustomer);
     while ($customerRow = mysqli_fetch_array($resultCustomer, MYSQLI_ASSOC)) {
         echo $resultCustomer["bizName"];
     }
+    mysqli_close($mysqli_link);
 }
 
 ?>
