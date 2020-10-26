2ea6cce65d186bceb3cda2dab0de4cddb7fbb9f5
Sean Snell <ssnell@cmhsol.com>
2020-10-26 17:07:20 -0400

    Added IP info section

--- server/resources/functions/shared_functions.php
+++ server/resources/functions/shared_functions.php
@@ -8,19 +8,36 @@ function displayCustomerName($custID, $mysqli_link) {
     }
 }
 
-function displayClientData($cid, $mysqli_link) {
+function displayClientDataSummary($cid, $mysqli_link) {
     $selectClientData = "SELECT * FROM clients WHERE cid='$cid'";
     $resultClientData = mysqli_query($mysqli_link, $selectClientData);
     while ($clientDataRow = mysqli_fetch_array($resultClientData, MYSQLI_ASSOC)) {
         echo '
-        <tr>
-            <td>Hostname</td>
-            <td align="right">'. $clientDataRow["hostname"] .'#</td>
-        </tr>
-        <tr>
-            <td>Status</td>
-            <td align="right"><span class="badge badge-success">Online</span></td>
-        </tr>
+            <tr>
+                <td>Hostname</td>
+                <td align="right">'. $clientDataRow["hostname"] .'</td>
+            </tr>
+            <tr>
+                <td>Status</td>
+                <td align="right"><span class="badge badge-success">Online</span></td>
+            </tr>
+        ';
+    }
+}
+
+function displayClientDataInt($cid, $mysqli_link) {
+    $selectClientData = "SELECT * FROM clients WHERE cid='$cid'";
+    $resultClientData = mysqli_query($mysqli_link, $selectClientData);
+    while ($clientDataRow = mysqli_fetch_array($resultClientData, MYSQLI_ASSOC)) {
+        echo '
+            <tr>
+                <td>WAN</td>
+                <td align="right">'. $clientDataRow["wanIP"] .'</td>
+            </tr>
+            <tr>
+                <td>LAN</td>
+                <td align="right">'. $clientDataRow["lanIP"] .'</td>
+            </tr>
         ';
     }
 }
