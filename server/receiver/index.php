08f944818ab43d66f6e763b28ef37eb4ae1241b9
Sean Snell <ssnell@cmhsol.com>
2020-10-28 15:23:00 -0400

    receiver updates

--- server/receiver/index.php
+++ server/receiver/index.php
@@ -1,17 +1,16 @@
 <?php
-
-$encodedVariables = $_GET['d'];
+if (isset($_GET['d'])) {
+    $encodedVariables = $_GET['d'];
+}
 
 function decodeURL($encodedVariables) {
     $decodeURL = convert_uudecode (base64_decode ($encodedVariables));
     return $decodeURL;
 }
 
-if (isset($_GET['test'])) {
-    $testVar = $_GET['test'];
-}
+
 
 $decodedURL = decodeURL($encodedVariables);
 
-echo $decodedURL;
+echo '[data]' .$decodedURL;
 ?>
